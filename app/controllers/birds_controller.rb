5fe5cb60c907c1938ed91326f35a7d9d77b23fef
Jake Manley <jbmanley45@gmail.com>
2022-12-07 09:38:45 -0800

    completed

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -22,10 +22,31 @@ class BirdsController < ApplicationController
     end
   end
 
+  def update 
+    bird = Bird.find_by(id: params[:id])
+    if bird 
+      bird.update(bird_params)
+      render json: bird
+    else 
+      render json: {error: "Birb not found"}, status: :not_found 
+    end
+  end
+  # custom routes break RESTful onventions. to keep RESTful- create new controller, such as Birds::LikesController, and add an update action in this controller
+  # whenever I have the inclination that I want to add a method on a controller that’s not part of the default five or whatever REST actions that we have by default, make a new controller! And just call it that.
+  def increment_likes 
+    bird = Bird.find_by(id: params[:id])
+    if bird 
+      bird.update(likes: bird.likes +1)
+      render json: bird 
+    else 
+      render json: {error: "Bird not found"}, status: :not_found 
+    end
+  end
+
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
 
 end
